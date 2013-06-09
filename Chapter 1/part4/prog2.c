8f86326d9ea15562b86a26c4835f7e58c601fc08
wilhelmpaulm <wilhelmpaulm@gmail.com>
2013-06-09 15:43:40 +0800

    added answers to the questions

---
+++ Chapter 1/part4/prog2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int nValue1;
+	printf("Enter a value:");
+	scanf("%f", &nValue1);
+	printf("Value1 is:%d \n", nValue1);
+	return 0;
+}
---
+++ Chapter 1/part4/prog3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float nValue1;
+printf("Enter a value:");
+scanf("%d", &nValue1);
+printf("Value1 is:%f \n", nValue1);
+printf("Value1 is:%d \n", nValue1);
+return 0;
+}
